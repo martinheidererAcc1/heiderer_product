d6b6619b9b75295eeb723c3faa5c4ed9a181d4b6
Heiderer2 <m.heiderer@gmx.net>
2020-04-18 22:20:23 +0200

    Class Company attribute name added

--- Company.java
+++ Company.java
@@ -3,9 +3,21 @@ import java.util.ArrayList;
 public class Company
 {
 	private ArrayList<Product> products;
+	private String name;
 	
-	public Company()
+	public Company(String name)
 	{
 		products = new ArrayList<Product>(100);
+		name = "IBM";
 	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}	
 }
